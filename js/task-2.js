8c353e0b8c5489c6e437d750d0e9625baa0ce602
Taras Nesterovskyi <mr.paleva@gmail.com>
2025-10-22 21:13:05 +0300

    1.1

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,18 @@
 class Storage {
+  #items;
   constructor(initialItems) {
-    this.items = initialItems;
+    this.#items = initialItems;
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    const itemIndex = this.items.indexOf(itemToRemove);
+    const itemIndex = this.#items.indexOf(itemToRemove);
     if (itemIndex !== -1) {
-      this.items.splice(itemIndex, 1);
+      this.#items.splice(itemIndex, 1);
     }
   }
 }
